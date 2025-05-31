493067aa5b3208f9e2613c7b2982984f44320fac
scyth0425 <scythe0425@naver.com>
2025-05-31 16:35:04 +0900

    update dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() {
   runApp(const MyApp());
@@ -30,6 +31,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     final List<String> entries = <String>['a','b','c','d'];
+    final List<String> links = <String>['https://example.com/a', 'https://example.com/b', 'https://example.com/c', 'https://example.com/d'];
     
     return Scaffold(
       appBar: AppBar(
@@ -41,10 +43,26 @@ class _MyHomePageState extends State<MyHomePage> {
         itemBuilder: (BuildContext context, int index) {
           return Card(
             color: Colors.white,
-            child: ListTile(
-              title: Text('제목: ${entries[index]}'),
-              subtitle: Text('링크: ${entries[index]}'),
-            ),
+            child: 
+            Column(
+              children: [
+                ListTile(
+                  title: Text('제목: ${entries[index]}'),
+                  subtitle: Text('링크: ${links[index]}'),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    TextButton(
+                      child: const Text('링크 방문'),
+                      onPressed: () {
+                      },
+                    ),
+                    const SizedBox(width: 8),
+                  ],
+                )
+              ]
+            )
           );
         },
       ),
